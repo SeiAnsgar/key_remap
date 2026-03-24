23b32bd3d647d0646ed971073a7e514c00107ab3
ansi <ansi@debDT.local>
2026-03-24 07:01:23 +0100

    add functionality

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <errno.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <linux/input.h>
 #include <linux/uinput.h>
-//  /dev/uinput
+
 
 
 void emit(int fd, int type, int code, int val)
@@ -19,28 +20,54 @@ void emit(int fd, int type, int code, int val)
     write(fd, &ie, sizeof(ie));
 }
 
-int main()
-{   
+int write_uinput()
+{
+//ioctl(fd_uinput);
     int fd_uinput = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+    if(fd_uinput < 0){
+        perror("Error opening /uinput");
+        return fd_uinput;
+    }
     struct uinput_setup usetup;
+    /*
+    struct input_id id;
+	char name[UINPUT_MAX_NAME_SIZE];
+	__u32 ff_effects_max;
+    */
 
-    ioctl(fd_uinput);
+    ioctl(fd_uinput, UI_SET_EVBIT, EV_KEY);
+    ioctl(fd_uinput, UI_SET_KEYBIT, KEY_B);
     
+    memset(&usetup, 0, sizeof(usetup));
+    usetup.id.bustype = BUS_USB;
+    usetup.id.vendor = 0x1234;
+    usetup.id.product = 0x5678;
+}
+
+int read_input()
+{
     //  /dev/input/event6
     int fd_input = open("/dev/input/event6", O_RDONLY);
-    //printf("%d\n",fd);
+    if(fd_input < 0){
+        perror("Error opening /input");
+        return fd_input;
+    }
     struct input_event ie;
     /*
     __u16 type;
 	__u16 code;
 	__s32 value;
     */
+
     while(1){
         read(fd_input, &ie, sizeof(ie));
-
         //printf("type: %d\n", ie.value);
         printf("code: %d\n", ie.code);
         //printf("value: %d\n", ie.value);
-    }
-    
+    }    
+}
+
+int main()
+{   
+    read_input();
 }
